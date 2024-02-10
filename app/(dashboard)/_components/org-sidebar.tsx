dcd9684abc351c3777fb3f14506696408ff6fdb5
Ali <alipalvane@gmail.com>
2024-02-10 21:06:53 +0330

    add org sidebar and switcher

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,77 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { LayoutDashboard, Star } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Logo from "@/public/logo.svg";
+
 export const OrgSidebar = () => {
+  const searchParams = useSearchParams();
+  const favorites = searchParams.get("favorites");
+
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      Org
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href="/">
+        <div className="flex items-center gap-x-3">
+          <Image src={Logo} alt="logo" height={60} width={60} />
+          <span className="font-semibold text-2xl">Myroy</span>
+        </div>
+      </Link>
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #e5e7eb",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+      <div className="space-y-1 w-full">
+        <Button
+          asChild
+          variant={favorites ? "ghost" : "secondary"}
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+            Team Boards
+          </Link>
+        </Button>
+        <Button
+          asChild
+          variant={favorites ? "secondary" : "ghost"}
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link
+            href={{
+              pathname: "/",
+              query: {
+                favorites: true,
+              },
+            }}
+          >
+            <Star className="h-4 w-4 mr-2" />
+            Favorite Boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -3,7 +3,7 @@ import ReactLoading from "react-loading";
 const Loading = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <ReactLoading type="balls" color="#212529" height={"100px"} width={"100px"} />
+      <ReactLoading type="balls" color="#6356E5" height={"80px"} width={"80px"} />
     </div>
   );
 };
