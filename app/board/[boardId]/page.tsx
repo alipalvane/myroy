5feb2142f118ee27e06e9ec459dd6a32ff3e3070
Ali <alipalvane@gmail.com>
2024-03-20 22:57:26 +0330

    liveblock config

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,12 +1,18 @@
+import { Room } from "@/components/room";
 import { Canvas } from "./_components/canvas";
+import { Loading } from "./_components/loading";
 
 interface BoardIdPageProps {
-    params:{
-        boardId: string
-    }
+  params: {
+    boardId: string;
+  };
 }
-const BoardIdPage = ({params}:BoardIdPageProps) => {
-  return <Canvas boardId={params.boardId}/>;
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  return (
+    <Room roomId={params.boardId} fallback={<Loading />}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
 };
 
 export default BoardIdPage;
